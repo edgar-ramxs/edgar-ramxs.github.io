6c7f8b7bfa44132a7ea94f56e33e8cd8905d981a
Edgar Ramos <edgararamosv@gmail.com>
2026-04-28 00:53:16 -0400

    config: update project configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
-import { defineConfig } from 'astro/config';
-import icon from 'astro-icon';
-
+import { defineConfig } from "astro/config";
+import icon from "astro-icon";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
 
 export default defineConfig({
-  site: 'https://edgar-ramxs.github.io',
+  site: "https://edgar-ramxs.github.io",
 
-  integrations: [
-    icon(),
-  ],
+  integrations: [icon()],
 
-  vite: {},
+  prefetch: false,
+
+  vite: {
+    resolve: {
+      alias: {
+        "@utils": join(dirname(fileURLToPath(import.meta.url)), "src/utils"),
+      },
+    },
+  },
 });
